d84ea06535046f9ffcb896dc6903d0f01fdd5b01
Theo Bulut <vertexclique@gmail.com>
2025-03-25 09:59:30 +0100

    Test

---
+++ astro.config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'astro/config';
+
+import expressiveCode from 'astro-expressive-code';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import spectre from './package/src';
+
+import node from '@astrojs/node';
+import { spectreDark } from './src/ec-theme';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://vertexclique.com',
+  output: 'static',
+  integrations: [
+    expressiveCode({
+      themes: [spectreDark],
+    }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: 'vertexclique',
+      openGraph: {
+        home: {
+          title: 'Vertexclique - Blog',
+          description: 'Ideas and thoughts in a sink.'
+        },
+        blog: {
+          title: 'Blog',
+          description: 'Braindumps here...'
+        },
+        projects: {
+          title: 'Projects'
+        }
+      },
+      // giscus: {
+      //   repository: 'louisescher/spectre',
+      //   repositoryId: 'R_kgDONjm3ig',
+      //   category: 'General',
+      //   categoryId: 'DIC_kwDONjm3is4ClmBF',
+      //   mapping: 'pathname',
+      //   strict: true,
+      //   reactionsEnabled: true,
+      //   emitMetadata: false,
+      //   lang: 'en',
+      // }
+    })
+  ],
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
+});
